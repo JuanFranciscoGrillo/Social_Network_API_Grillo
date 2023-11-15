5140852d34eb8f6ab3e59ce66d3c61f76cb07e1a
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-15 05:48:35 -0400

    Defined Thought model

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+// models/Thought.js
+const mongoose = require('mongoose');
+const ReactionSchema = require('./Reaction'); // Assuming Reaction schema is defined in Reaction.js
+
+const thoughtSchema = new mongoose.Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: timestamp => new Date(timestamp).toISOString()
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [ReactionSchema]
+}, {
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
